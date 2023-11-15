4f2c0f18aa056ce3750292d2c10005f0da974528
callmemaid <150941971+callmemaid@users.noreply.github.com>
2023-11-15 17:04:46 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// You can add JavaScript code here for interactivity
+// For simplicity, let's add a basic example
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Example: Change the background color when the page loads
+    document.body.style.backgroundColor = '#f0f0f0';
+});
